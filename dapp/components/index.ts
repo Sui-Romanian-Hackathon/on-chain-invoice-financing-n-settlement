1b49f0cadb9aea978adeec1dd8c9e60519d2f429
paulAlexSerban <paul.alex.serban@gmail.com>
2025-11-14 23:50:57 +0200

    feat: split homepage

--- dapp/components/index.ts
+++ dapp/components/index.ts
@@ -19,6 +19,13 @@ export { default as InvestmentList } from "./InvestmentList";
 export { default as PortfolioDistribution } from "./PortfolioDistribution";
 export { default as PerformanceMetrics } from "./PerformanceMetrics";
 
+// Landing Page Components
+export { default as HeroSection } from "./HeroSection";
+export { default as StatsSection } from "./StatsSection";
+export { default as FeaturesSection } from "./FeaturesSection";
+export { default as HowItWorksSection } from "./HowItWorksSection";
+export { default as CTASection } from "./CTASection";
+
 // Existing Components
 export { default as Navigation } from "./Navigation";
 export { default as StatsOverview } from "./StatsOverview";
@@ -34,3 +41,6 @@ export type { Investment } from "./InvestmentCard";
 export type { PortfolioStats } from "./PortfolioStatsCards";
 export type { DistributionItem } from "./PortfolioDistribution";
 export type { MetricItem } from "./PerformanceMetrics";
+export type { StatItem } from "./StatsSection";
+export type { Feature } from "./FeaturesSection";
+export type { Step } from "./HowItWorksSection";
